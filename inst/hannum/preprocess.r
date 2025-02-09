c7dd41b164f67ca71b5688ddd72e04ec186f6eee
Matthew Suderman <matthewsuderman@hotmail.com>
2025-02-09 16:19:49 +0000

    hannum preprocess

--- inst/hannum/preprocess.r
+++ inst/hannum/preprocess.r
@@ -2,3 +2,5 @@ x <- read.csv("hannum-model.csv",stringsAsFactors=F)
 coefs <- x[,c("Marker","Coefficient")]
 colnames(coefs) <- c("pred.var","coef")
 write.csv(coefs, file="coefs.csv",row.names=F)
+
+
